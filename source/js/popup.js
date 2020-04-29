71ebe0f5ba2829f734d2b196e8eeb07cff31ad91
Margo <rrrita123@mail.ru>
2020-04-29 20:55:27 +0400

    Исправление js

--- source/js/popup.js
+++ source/js/popup.js
@@ -1,16 +1,8 @@
+"use strict";
+
 var buttonOpen = document.querySelector(".page-header__button");
 var menu = document.querySelector(".main-nav");
 var pageHeader = document.querySelector(".page-header");
-var form = document.querySelector(".form-review");
-var nameTourist = form.querySelector("#name");
-var surnameTourist = form.querySelector("#surname");
-var telTourist = form.querySelector("#tel");
-var emailTourist = form.querySelector("#email");
-var messageError = document.querySelector(".info-inner__error");
-var messageSabmit = document.querySelector(".info-inner__message");
-var buttonForm = document.querySelector(".button-form");
-var buttonPopup = document.querySelector(".button-form__popup");
-
 
 menu.classList.add("main-nav--closed");
 buttonOpen.classList.add("page-header__toggle");
@@ -28,39 +20,56 @@ buttonOpen.addEventListener("click", function(evt) {
   }
 });
 
-form.addEventListener("submit", function(evt) {
+// Проверка заполнения обязательных полей в форме
+if(document.querySelector(".form-review")) {
+  var form = document.querySelector(".form-review");
+  var nameTourist = form.querySelector("#name");
+  var surnameTourist = form.querySelector("#surname");
+  var telTourist = form.querySelector("#tel");
+  var emailTourist = form.querySelector("#email");
+  var messageError = document.querySelector(".info-inner__error");
+  var messageSabmit = document.querySelector(".info-inner__message");
+  var buttonForm = document.querySelector(".button-form");
+  var buttonPopup = document.querySelector(".button-form__popup");
+  var collectionInput = form.querySelectorAll(".js-input");
 
-  var valid = true;
-
-  if (!nameTourist.value) {
-    valid = false;
-    nameTourist.classList.add("form-review__input--error");
-  }
-
-  if (!surnameTourist.value) {
-    valid = false;
-    surnameTourist.classList.add("form-review__input--error");
-  }
-
-  if (!telTourist.value) {
-    valid = false;
-    telTourist.classList.add("form-review__input--error");
-  }
-
-  if (!emailTourist.value) {
-    valid = false;
-    emailTourist.classList.add("form-review__input--error");
-  }
-
-  if (valid) {
-    messageSabmit.classList.add("info-inner--show");
-  }
-  else {
+  form.addEventListener("submit", function(evt) {
     evt.preventDefault();
-    messageError.classList.add("info-inner--show");
-  }
-});
+    var valid = true;
 
-buttonPopup.addEventListener("click", function(evt) {
-  messageError.classList.remove("info-inner--show");
-});
+    collectionInput.forEach((i) => {
+      i.required = false;
+      if (!i.value) {
+        valid = false;
+        i.classList.add("form-review__input--error");
+      } else {
+        i.classList.remove("form-review__input--error");
+      }
+    })
+
+    if (valid) {
+      messageSabmit.classList.add("info-inner--show");
+    }
+    else {
+      messageError.classList.add("info-inner--show");
+    }
+  });
+
+  buttonPopup.addEventListener("click", function(evt) {
+    messageError.classList.remove("info-inner--show");
+  });
+}
+
+//Create map
+var map;
+function initMap() {
+  map = new google.maps.Map(
+    document.getElementById('map'),
+    {center: new google.maps.LatLng(34.961755, -111.758092), zoom: 7}
+  );
+
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(34.961755, -111.758092),
+    map: map
+  });
+}
